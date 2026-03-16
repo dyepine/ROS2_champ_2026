f5803214aa3dc23ccc502b6f0ec6ca78289db4e5
Mikle11121 <mihail.betelev@yandex.ru>
2026-03-16 22:31:15 +0300

    jpeg files for calibrate

--- src/ros2/camera_localization/camera_localization/camera_odom.py
+++ src/ros2/camera_localization/camera_localization/camera_odom.py
@@ -5,8 +5,6 @@ from geometry_msgs.msg import PoseStamped, TransformStamped, PoseWithCovarianceS
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
-
-
 class MapToOdomTF(Node):
     def __init__(self):
         super().__init__("camera_odom")
@@ -22,25 +20,37 @@ class MapToOdomTF(Node):
         )
         
         self.tf_broadcaster = tf2_ros.TransformBroadcaster(self)
-        self.inital_pose_coords = None
-        self.inital_pose_rotation = None
+        self.initial_pose_coords = None
+        self.initial_pose_rotation = None
+        
+        # Добавляем publisher для позиций объектов
+        self.object_pose_pub = self.create_publisher(
+            PoseStamped, '/object_poses', 10
+        )
 
     def odom_calculation(self, msg):
-        if self.inital_pose_coords is not None:
+        if self.initial_pose_coords is not None:
             odom_to_base_footprint = TransformStamped()
             odom_to_base_footprint.header.stamp = self.get_clock().now().to_msg()
             odom_to_base_footprint.header.frame_id = "odom"
             odom_to_base_footprint.child_frame_id = "aruco_predict"
 
-            map_coodrinats = np.array([msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z])
-            map_rotation = np.array((R.from_quat(np.array([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]))).as_matrix())
+            map_coordinates = np.array([msg.pose.pose.position.x, 
+                                        msg.pose.pose.position.y, 
+                                        msg.pose.pose.position.z])
+            map_rotation = np.array((R.from_quat(np.array([
+                msg.pose.pose.orientation.x, 
+                msg.pose.pose.orientation.y, 
+                msg.pose.pose.orientation.z, 
+                msg.pose.pose.orientation.w
+            ])).as_matrix()))
 
-            odom_to_base_footprint_coodrs = np.linalg.inv(self.inital_pose_rotation) @ (map_coodrinats-self.inital_pose_coords)
-            odom_to_base_footprint_quat = (R.from_matrix(map_rotation @ np.linalg.inv(self.inital_pose_rotation))).as_quat()
+            odom_to_base_footprint_coords = np.linalg.inv(self.initial_pose_rotation) @ (map_coordinates - self.initial_pose_coords)
+            odom_to_base_footprint_quat = (R.from_matrix(map_rotation @ np.linalg.inv(self.initial_pose_rotation))).as_quat()
 
-            odom_to_base_footprint.transform.translation.x = odom_to_base_footprint_coodrs[0]
-            odom_to_base_footprint.transform.translation.y = odom_to_base_footprint_coodrs[1]
-            odom_to_base_footprint.transform.translation.z = odom_to_base_footprint_coodrs[2]
+            odom_to_base_footprint.transform.translation.x = odom_to_base_footprint_coords[0]
+            odom_to_base_footprint.transform.translation.y = odom_to_base_footprint_coords[1]
+            odom_to_base_footprint.transform.translation.z = odom_to_base_footprint_coords[2]
 
             odom_to_base_footprint.transform.rotation.x = odom_to_base_footprint_quat[0]
             odom_to_base_footprint.transform.rotation.y = odom_to_base_footprint_quat[1]
@@ -49,13 +59,16 @@ class MapToOdomTF(Node):
             
             self.tf_broadcaster.sendTransform(odom_to_base_footprint)
 
-            # self.get_logger().info(
-            #     f"odom pose set: x={initial_x}, y={initial_y}, z={initial_z}"
-            # )
-
     def initial_pose_initialisation(self, msg):
-        self.inital_pose_coords = np.array([msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z])
-        self.inital_pose_rotation = np.array((R.from_quat(np.array([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]))).as_matrix())
+        self.initial_pose_coords = np.array([msg.pose.pose.position.x, 
+                                            msg.pose.pose.position.y, 
+                                            msg.pose.pose.position.z])
+        self.initial_pose_rotation = np.array((R.from_quat(np.array([
+            msg.pose.pose.orientation.x, 
+            msg.pose.pose.orientation.y, 
+            msg.pose.pose.orientation.z, 
+            msg.pose.pose.orientation.w
+        ])).as_matrix()))
 
     def enemy_pose(self, msg):
         map_to_enemy = TransformStamped()
@@ -74,14 +87,11 @@ class MapToOdomTF(Node):
 
         self.tf_broadcaster.sendTransform(map_to_enemy)
 
-
-
 def main(args=None):
     rclpy.init(args=args)
     node = MapToOdomTF()
     rclpy.spin(node)
     rclpy.shutdown()
-
 
 if __name__ == "__main__":
     main()
